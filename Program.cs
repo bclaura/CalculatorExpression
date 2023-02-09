2acf3c2332f999751d8b4a04062de6f4fe6e91f8
brost <brost@DESKTOP-RFOM31U>
2023-02-09 14:45:58 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.JScript.Vsa;
+using Microsoft.JScript;
+
+namespace CalculatorExpression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculator" + "\n");
+            Console.WriteLine("Calculate your problem here: ");
+            while (true)
+            {
+                var userInput = Console.ReadLine();
+                var engine = VsaEngine.CreateEngine();
+                var result = Eval.JScriptEvaluate(userInput, engine);
+
+                Console.WriteLine("The result is: " + result + "\n");
+                Console.WriteLine("Enter another one: ");
+            }
+        }
+    }
+}
+
+
